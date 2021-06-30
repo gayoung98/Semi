7e4e4a24e5e9cae94c299b0f5b531500451d1b6d
dayikim <wi0910@naver.com>
2021-06-30 16:49:24 +0900

    free/noticeDTO added

---
+++ src/dto/FreeCommentDTO.java
@@ -0,0 +1,82 @@
+package dto;
+
+import java.sql.Date;
+
+public class FreeCommentDTO {
+	
+	
+	private int seq;
+	private String id;
+
+    private String writer;
+
+    private String comments;
+
+    private Date write_date;
+
+    private int parent;
+    
+    public FreeCommentDTO() {}
+    
+    public FreeCommentDTO(int seq, String id, String writer, String comments, Date write_date, int parent) {
+	this.seq = seq;
+	this.id = id;
+	this.writer = writer;
+	this.comments = comments;
+	this.write_date = write_date;
+	this.parent = parent;
+}
+
+	
+	 public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public Date getWrite_date() {
+		return write_date;
+	}
+
+	public void setWrite_date(Date write_date) {
+		this.write_date = write_date;
+	}
+
+	public int getParent() {
+		return parent;
+	}
+
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+
+	
+	    
+	    
+}
