c7a3bd9ad0326e9ae097347731b00e69430b31a2
hansol <njwjsgksthf@naver.com>
2021-07-02 10:00:59 +0900

    dto update

--- src/dto/AssDTO.java
+++ src/dto/AssDTO.java
@@ -3,19 +3,26 @@ package dto;
 import java.sql.Date;
 
 public class AssDTO {
-
+//	create table ASS(
+//		    seq number primary key,
+//		    writer varchar2(15) not null,
+//		    id varchar2(20) not null,
+//		    title varchar2(50) not null,
+//		    contents varchar2(4000) not null,
+//		    write_date date default sysdate not null,
+//		    viewCount number default 0 not null
+//		);
+	
 	private int seq;
 	private String writer;
 	private String id;
 	private String title;
 	private String contents;
 	private Date write_date;
+	private int viewCount;
 	
-	public AssDTO() {
-		super();
-	}
-
-	public AssDTO(int seq, String writer, String id, String title, String contents, Date write_date) {
+	public AssDTO() {}
+	public AssDTO(int seq, String writer, String id, String title, String contents, Date write_date, int viewCount) {
 		super();
 		this.seq = seq;
 		this.writer = writer;
@@ -23,56 +30,50 @@ public class AssDTO {
 		this.title = title;
 		this.contents = contents;
 		this.write_date = write_date;
+		this.viewCount = viewCount;
 	}
-
 	public int getSeq() {
 		return seq;
 	}
-
 	public void setSeq(int seq) {
 		this.seq = seq;
 	}
-
 	public String getWriter() {
 		return writer;
 	}
-
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getContents() {
 		return contents;
 	}
-
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
-
 	public Date getWrite_date() {
 		return write_date;
 	}
-
 	public void setWrite_date(Date write_date) {
 		this.write_date = write_date;
 	}
-
-		
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	
 	
 }
--- src/dto/inquiredDTO.java
+++ src/dto/inquiredDTO.java
@@ -5,24 +5,26 @@ import java.sql.Date;
 public class inquiredDTO {
    
    private int seq; 
-   private String reporter;
+   private String id;
    private String major_category;
    private String sub_category;
    private String contents;
+   private String recomment;
    private Date reg_date;
-   public inquiredDTO(int seq, String reporter, String major_category, String sub_category, String contents,
+   public inquiredDTO(int seq, String id, String major_category, String sub_category, String contents,String recomment,
          Date reg_date) {
       super();
       this.seq = seq;
-      this.reporter = reporter;
+      this.id = id;
       this.major_category = major_category;
       this.sub_category = sub_category;
       this.contents = contents;
+      this.recomment=recomment;
       this.reg_date = reg_date;
    }
-   public inquiredDTO(String reporter, String major_category, String sub_category, String contents) {
+   public inquiredDTO(String id, String major_category, String sub_category, String contents) {
       super();
-      this.reporter = reporter;
+      this.id = id;
       this.major_category = major_category;
       this.sub_category = sub_category;
       this.contents = contents;
@@ -33,13 +35,14 @@ public class inquiredDTO {
    public void setSeq(int seq) {
       this.seq = seq;
    }
-   public String getReporter() {
-      return reporter;
-   }
-   public void setReporter(String reporter) {
-      this.reporter = reporter;
-   }
-   public String getMajor_category() {
+  
+   public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getMajor_category() {
       return major_category;
    }
    public void setMajor_category(String major_category) {
@@ -57,7 +60,14 @@ public class inquiredDTO {
    public void setContents(String contents) {
       this.contents = contents;
    }
-   public Date getReg_date() {
+   
+   public String getRecomment() {
+	return recomment;
+}
+public void setRecomment(String recomment) {
+	this.recomment = recomment;
+}
+public Date getReg_date() {
       return reg_date;
    }
    public void setReg_date(Date reg_date) {
