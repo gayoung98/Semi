8563fd3f9ad32a92f6e2da40870fbc74e8930f3a
hansol <njwjsgksthf@naver.com>
2021-07-09 17:28:17 +0900

    today last

--- src/quartz/ResetJob.java
+++ src/quartz/ResetJob.java
@@ -2,23 +2,30 @@ package quartz;
 
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
 
 import dao.ManagerDAO;
 
 public class ResetJob implements Job {
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
+		Logger l = Logger.getLogger(ResetJob.class);
 		ManagerDAO managerDao = ManagerDAO.getInstance();    
 		try {
 		int result1 = managerDao.deleteChat();
 		int result2 = managerDao.deleteSeat();
 		if(result1>0 && result2>0) {
-			System.out.println("데이터 삭제 성공");
+			l.warn("자리신청, 채팅 데이터 삭제 성공");
+		}else if(result1>0 && result2<1) {
+			l.warn("채팅 데이터 삭제 성공");
+		}else if(result1<1 && result2>0) {
+			l.warn("자리신청 데이터 삭제 성공");
 		}else {
-			System.out.println("데이터 삭제 실패");
+			l.warn("데이터 삭제 실패");
 		}
 		}catch(Exception e) {
 			
