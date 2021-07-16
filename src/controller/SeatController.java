7bca5d04a8ac80cf62151e95e728fed58af3144d
hansol <njwjsgksthf@naver.com>
2021-07-16 14:34:59 +0900

    seat

--- src/controller/SeatController.java
+++ src/controller/SeatController.java
@@ -19,121 +19,121 @@ import dto.SeatDTO;
 
 @WebServlet("*.seat")
 public class SeatController extends HttpServlet {
-	private final ReentrantLock re = new ReentrantLock();
+   private final ReentrantLock re = new ReentrantLock();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset= utf-8");
-		try {
+      request.setCharacterEncoding("utf-8");
+      response.setCharacterEncoding("utf-8");
+      response.setContentType("text/html; charset= utf-8");
+      try {
 
-			String requestURI = request.getRequestURI();
-			String ctxPath = request.getContextPath();
-			String url = requestURI.substring(ctxPath.length());
-			SeatDAO dao = SeatDAO.getInstance();
-			MemberDAO memdao = MemberDAO.getInstance();
-			System.out.println(url);
-			if(url.contentEquals("/reserve2.seat")){
-				re.lock();
-				String date  = request.getParameter("date");
-				System.out.println((String)request.getParameter("seatNumber"));
-				System.out.println((String)request.getParameter("cancelSeat"));
-				String email = (String) request.getSession().getAttribute("login");
-				MemberDTO dto = memdao.getMainInfo(email);
-				String name =  dto.getName();
-
-
-				System.out.println("date: " + date);
-
-				int count = dao.rownum(date);
-				boolean already = dao.isReserved(email,date);
-				if(request.getParameter("seatNumber")!=null) {
-					String seat_number = (String)request.getParameter("seatNumber");
-					boolean mySeat = dao.mySeat(email, seat_number, date);
-					boolean conflict = dao.conflict(seat_number);
-					if(count < 14) {
-						if(already == false && conflict == false) {
-							int deleteSame = dao.deleteSame(dao.min(name, seat_number), name, seat_number);
-							dao.insert(date, email, name, (String)request.getParameter("seatNumber"));
-							deleteSame = dao.deleteSame(dao.min(name, seat_number), name, seat_number);
-							response.getWriter().append(request.getParameter("seatNumber"));
-						
-					}else {
-						//�씠誘� �꽑�깮�뻽�뒗�뜲 �떎瑜� 醫뚯꽍 �늻瑜쇰븣
-						response.getWriter().append("already");
-					}
-					}
-				}else {
-					//14紐� �떊泥��뻽�쓣�븣
-					response.getWriter().append("corona");
-				}
-
-				re.unlock();
-			} else if(url.contentEquals("/complete.seat")) {
-				Gson gs =new Gson(); 
-				String date  = request.getParameter("date");
-				//				date = request.getParameter("date");
-				//				System.out.println("date: " + date);
-
-				String email = (String)request.getSession().getAttribute("login");
-				MemberDTO dto = memdao.getMainInfo(email);
-				String khclass = dto.getKhClass();
-				String branch = dto.getBranch();
-				String name = dto.getName();
-
-				String mySeatNumber = dao.mySeatNumber(date, name);
-				if(mySeatNumber != null ) dao.deleteSame(dao.min(name, mySeatNumber), name, mySeatNumber);
-				for(SeatDTO sd:dao.classList(date, khclass, branch) ) {
-					System.out.println(sd.getEmail());
-				}
-				if(dao.classList(date, khclass, branch).size() == 0) {
-					response.getWriter().append(gs.toJson(date));
-				}else {
-					response.getWriter().append(gs.toJson(dao.classList(date, khclass, branch)));
-					/* request.getRequestDispatcher("seat/seat.jsp").forward(request, response); */
-				}
-			}else if(url.contentEquals("/date.seat")) {
-				String date  = request.getParameter("date");
-				request.setAttribute("date", date);
-				request.getRequestDispatcher("kh/seat/seat.jsp").forward(request, response);
-
-			}else if(url.contentEquals("/seat.seat")) {
-				String date  = request.getParameter("date");
-				request.getRequestDispatcher("kh/seat/seat.jsp").forward(request, response);
-			}else if(url.contentEquals("/cancel.seat")) {
-				String date  = request.getParameter("date");
-				System.out.println((String)request.getParameter("seatNumber"));
-				System.out.println((String)request.getParameter("cancelSeat"));
-				String email = (String) request.getSession().getAttribute("login");
-				MemberDTO dto = memdao.getMainInfo(email);
-				String name =  dto.getName();
-
-				System.out.println("date: " + date);
-
-				if(request.getParameter("cancelSeat")!=null) {
-
-					String seat_number = (String)request.getParameter("cancelSeat");
-					boolean mySeat = dao.mySeat(email, seat_number, date);
-					if(mySeat == true) {
-						dao.delete(email, request.getParameter("cancelSeat"));
-						response.getWriter().append(request.getParameter("cancelSeat"));
-					}else {
-						//�떎瑜� �궗�엺�씠 �떊泥��븳 醫뚯꽍 �늻瑜쇰븣
-						response.getWriter().append("notmyseat");
-					}
-				}
-			}
+         String requestURI = request.getRequestURI();
+         String ctxPath = request.getContextPath();
+         String url = requestURI.substring(ctxPath.length());
+         SeatDAO dao = SeatDAO.getInstance();
+         MemberDAO memdao = MemberDAO.getInstance();
+         System.out.println(url);
+         if(url.contentEquals("/reserve2.seat")){
+            re.lock();
+            String date  = request.getParameter("date");
+            System.out.println((String)request.getParameter("seatNumber"));
+            System.out.println((String)request.getParameter("cancelSeat"));
+            String email = (String) request.getSession().getAttribute("login");
+            MemberDTO dto = memdao.getMainInfo(email);
+            String name =  dto.getName();
 
 
-		}catch(Exception e) {
-			e.printStackTrace();
-			response.sendRedirect("error.jsp");
-		}
-	}
+            System.out.println("date: " + date);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+            int count = dao.rownum(date);
+            boolean already = dao.isReserved(email,date);
+            if(request.getParameter("seatNumber")!=null) {
+               String seat_number = (String)request.getParameter("seatNumber");
+               boolean mySeat = dao.mySeat(email, seat_number, date);
+               boolean conflict = dao.conflict(seat_number);
+               if(count < 14) {
+                  if(already == false && conflict == false) {
+                     int deleteSame = dao.deleteSame(dao.min(name, seat_number), name, seat_number);
+                     dao.insert(date, email, name, (String)request.getParameter("seatNumber"));
+                     deleteSame = dao.deleteSame(dao.min(name, seat_number), name, seat_number);
+                     response.getWriter().append(request.getParameter("seatNumber"));
+                  
+               }else {
+                  // 씠誘   꽑 깮 뻽 뒗 뜲  떎瑜  醫뚯꽍  늻瑜쇰븣
+                  response.getWriter().append("already");
+               }
+               }
+            }else {
+               //14紐   떊泥  뻽 쓣 븣
+               response.getWriter().append("corona");
+            }
+
+            re.unlock();
+         } else if(url.contentEquals("/complete.seat")) {
+            Gson gs =new Gson(); 
+            String date  = request.getParameter("date");
+            //            date = request.getParameter("date");
+            //            System.out.println("date: " + date);
+
+            String email = (String)request.getSession().getAttribute("login");
+            MemberDTO dto = memdao.getMainInfo(email);
+            String khclass = dto.getKhClass();
+            String branch = dto.getBranch();
+            String name = dto.getName();
+
+            String mySeatNumber = dao.mySeatNumber(date, name);
+            if(mySeatNumber != null ) dao.deleteSame(dao.min(name, mySeatNumber), name, mySeatNumber);
+            for(SeatDTO sd:dao.classList(date, khclass, branch) ) {
+               System.out.println(sd.getEmail());
+            }
+            if(dao.classList(date, khclass, branch).size() == 0) {
+               response.getWriter().append(gs.toJson(date));
+            }else {
+               response.getWriter().append(gs.toJson(dao.classList(date, khclass, branch)));
+               /* request.getRequestDispatcher("seat/seat.jsp").forward(request, response); */
+            }
+         }else if(url.contentEquals("/date.seat")) {
+            String date  = request.getParameter("date");
+            request.setAttribute("date", date);
+            request.getRequestDispatcher("kh/seat/seat.jsp").forward(request, response);
+
+         }else if(url.contentEquals("/seat.seat")) {
+            String date  = request.getParameter("date");
+            request.getRequestDispatcher("kh/seat/seat.jsp").forward(request, response);
+         }else if(url.contentEquals("/cancel.seat")) {
+            String date  = request.getParameter("date");
+            System.out.println((String)request.getParameter("seatNumber"));
+            System.out.println((String)request.getParameter("cancelSeat"));
+            String email = (String) request.getSession().getAttribute("login");
+            MemberDTO dto = memdao.getMainInfo(email);
+            String name =  dto.getName();
+
+            System.out.println("date: " + date);
+
+            if(request.getParameter("cancelSeat")!=null) {
+
+               String seat_number = (String)request.getParameter("cancelSeat");
+               boolean mySeat = dao.mySeat(email, seat_number, date);
+               if(mySeat == true) {
+                  dao.delete(email, request.getParameter("cancelSeat"));
+                  response.getWriter().append(request.getParameter("cancelSeat"));
+               }else {
+                  // 떎瑜   궗 엺 씠  떊泥  븳 醫뚯꽍  늻瑜쇰븣
+                  response.getWriter().append("notmyseat");
+               }
+            }
+         }
+
+
+      }catch(Exception e) {
+         e.printStackTrace();
+         response.sendRedirect("error.jsp");
+      }
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
 
 }
